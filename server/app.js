abf4573a16cc6eb5b3fb67ac5bafc2090d890ae0
Ben Purdy <ben.purdy@gmail.com>
2016-06-11 13:24:43 -0700

    Added headers to allow cross-domain posts.

--- server/app.js
+++ server/app.js
@@ -234,6 +234,10 @@ app.use( (req, res, next ) => {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
+  
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, slot-key');
+
   next();
 });
 app.use(express.static('../'));
